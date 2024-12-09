14d2230c3872df2b67520b575ac462c131e95741
YuliiaKovalova <ykovalova@microsoft.com>
2024-12-09 11:19:52 +0100

    Add demo and presentation

---
+++ DemoMSBuildMeetup/CustomMSBuildTask/CustomMSBuildTask.cs
@@ -0,0 +1,14 @@
+﻿using Task = Microsoft.Build.Utilities.Task;
+
+namespace MyTasks
+{
+    public class CustomMSBuildTask : Task
+    {
+        public override bool Execute()
+        {
+            Log.LogError("Hello from Prague .Net Meetup!");
+
+            return true;
+        }
+    }
+}
